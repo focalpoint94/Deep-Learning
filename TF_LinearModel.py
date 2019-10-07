0b23d283a4313ed302064be83d8f8bb2d686c960
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2019-10-07 21:07:32 +0900

    Update TF_LinearModel.py

--- TF_LinearModel.py
+++ TF_LinearModel.py
@@ -15,13 +15,9 @@ print("- Training-set:\t\t{}".format(data.num_train))
 print("- Validation-set:\t{}".format(data.num_val))
 print("- Test-set:\t\t{}".format(data.num_test))
 
-# The images are stored in one-dimensional arrays of this length.
 img_size_flat = data.img_size_flat
 
-# Tuple with height and width of images used to reshape arrays.
 img_shape = data.img_shape
-
-# Number of classes, one class for each of 10 digits.
 num_classes = data.num_classes
 
 import functools
@@ -63,7 +59,6 @@ class Model:
         self.y_pred = tf.nn.softmax(self.logits)
         y_pred_cls = tf.argmax(self.y_pred, axis = 1)
         return y_pred_cls
-        #return tf.argmax(y_pred, axis = 1)
     
     @lazy_property
     def optimize(self):
@@ -84,53 +79,29 @@ def plot_images2(images, cls_true, cls_pred, logits, pred):
     assert len(images) == len(cls_true) == 9
     
     plt.rcParams["figure.figsize"] = (16,16)
-    # Create figure with 3x3 sub-plots.
     fig, axes = plt.subplots(3, 3)
     fig.subplots_adjust(hspace=0.3, wspace=0.3)
 
     for i, ax in enumerate(axes.flat):
-        # Plot image.
-        ax.imshow(images[i].reshape(img_shape), cmap='binary')
-
-        # Show true and predicted classes.
+        ax.imshow(images[i].reshape(img_shape), cmap='binary'
         xlabel = "True: {0:1d}, Pred: {1:1d}, logits: {2:.2f}, y_pred: {3:.2f}".format(cls_true[i], cls_pred[i], 
                                                                                np.max(logits[i]), np.max(pred[i]))
-
         ax.set_xlabel(xlabel)
-        
-        # Remove ticks from the plot.
         ax.set_xticks([])
         ax.set_yticks([])
-        
-    # Ensure the plot is shown correctly with multiple plots
-    # in a single Notebook cell.
+      
     plt.show()
 
 def plot_example_errors2():
-    # Use TensorFlow to get a list of boolean values
-    # whether each test-image has been correctly classified,
-    # and a list for the predicted class of each image.
-    
     correct, cls_pred, logits, pred = session.run([model.correct_prediction, model.prediction, model.logits, model.y_pred],
                                     feed_dict=feed_dict_test)
-    
-    # Negate the boolean array.
     incorrect = (correct == False)
-    
-    # Get the images from the test-set that have been
-    # incorrectly classified.
     images = data.x_test[incorrect]
-    
-    # Get the predicted classes for those images.
     cls_pred = cls_pred[incorrect]
-
-    # Get the true classes for those images.
     cls_true = data.y_test_cls[incorrect]
     
     logits = logits[incorrect]
     pred = pred[incorrect]
-    
-    # Plot the first 9 images.
     plot_images2(images=images[0:9],
                 cls_true=cls_true[0:9],
                 cls_pred=cls_pred[0:9],
@@ -142,7 +113,6 @@ num_steps = 100
     
 tf.reset_default_graph()
 
-# TODO : Model object construction
 x = tf.placeholder(tf.float32, [None, img_size_flat])
 y_true = tf.placeholder(tf.float32, [None, num_classes])
 y_true_cls = tf.placeholder(tf.int64, [None])
@@ -152,7 +122,6 @@ with tf.Session() as session:
     session.run(tf.global_variables_initializer())
     
     for step in range(num_steps):
-        # TODO : Model Optimization
         x_batch, y_true_batch, _ = data.random_batch(batch_size=batch_size)
         feed_dict_train = {x: x_batch, y_true: y_true_batch}
         session.run(model.optimize, feed_dict_train)
@@ -189,7 +158,6 @@ class Model2:
         self.y_pred = tf.nn.softmax(self.logits)
         y_pred_cls = tf.argmax(self.y_pred, axis = 1)
         return y_pred_cls
-        #return tf.argmax(y_pred, axis = 1)
     
     @lazy_property
     def optimize(self):
@@ -211,7 +179,6 @@ num_steps = 500
     
 tf.reset_default_graph()
 
-# TODO : Model object construction
 x = tf.placeholder(tf.float32, [None, img_size_flat])
 y_true = tf.placeholder(tf.float32, [None, num_classes])
 y_true_cls = tf.placeholder(tf.int64, [None])
@@ -221,7 +188,6 @@ with tf.Session() as session:
     session.run(tf.global_variables_initializer())
     
     for step in range(num_steps):
-        # TODO : Model Optimization
         x_batch, y_true_batch, _ = data.random_batch(batch_size=batch_size)
         feed_dict_train = {x: x_batch, y_true: y_true_batch}
         session.run(model.optimize, feed_dict_train)
