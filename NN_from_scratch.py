ba9500e583b314f7e0a73218e358be7524840908
Hoesu Chun <55021961+focalpoint94@users.noreply.github.com>
2019-10-07 20:48:27 +0900

    Create NN_from_scratch.py

---
+++ NN_from_scratch.py
@@ -0,0 +1,101 @@
+from __future__ import print_function
+import numpy as np
+#import tensorflow as tf
+from six.moves import cPickle as pickle
+from six.moves import range
+import matplotlib
+import matplotlib.pyplot as plt
+
+%matplotlib inline
+
+
+num_examples = len(train_dataset) # training set size
+nn_input_dim = 784 # input layer dimensionality
+nn_output_dim = 10 # output layer dimensionality
+epsilon = 0.01 # learning rate for gradient descent
+reg_lambda = 0.01 # regularization strength
+
+def calculate_loss(model):
+    W1, b1, W2, b2, W3, b3 = model['W1'], model['b1'], model['W2'], model['b2'], model['W3'], model['b3']
+    z1 = train_dataset.dot(W1) + b1
+    a1 = np.tanh(z1)
+    z2 = a1.dot(W2) + b2
+    a2 = np.tanh(z2)
+    z3 = a2.dot(W3) + b3
+    exp_scores = np.exp(z3)
+    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+    correct_logprobs = -np.log([probs[i, np.nonzero(train_labels)[(1)][i].astype('int64')] for i in range(num_examples)])
+    data_loss = np.sum(correct_logprobs)
+    data_loss += reg_lambda/3 * (np.sum(np.square(W1)) + np.sum(np.square(W2)) + np.sum(np.square(W3)))
+    return 1./num_examples * data_loss
+    
+def predict(model, x):
+    W1, b1, W2, b2, W3, b3 = model['W1'], model['b1'], model['W2'], model['b2'], model['W3'], model['b3']
+    z1 = x.dot(W1) + b1
+    a1 = np.tanh(z1)
+    z2 = a1.dot(W2) + b2
+    a2 = np.tanh(z2)
+    z3 = a2.dot(W3) + b3
+    exp_scores = np.exp(z3)
+    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+    return np.argmax(probs, axis=1)
+    
+def build_model(nn_hdim, num_passes=20000, print_loss=False):
+    np.random.seed(0)
+    W1 = np.random.randn(nn_input_dim, nn_hdim) / np.sqrt(nn_input_dim)
+    b1 = np.zeros((1, nn_hdim))
+    W2 = np.random.randn(nn_hdim, nn_hdim) / np.sqrt(nn_hdim)
+    b2 = np.zeros((1, nn_hdim))    
+    W3 = np.random.randn(nn_hdim, nn_output_dim) / np.sqrt(nn_hdim)
+    b3 = np.zeros((1, nn_output_dim))
+
+    model = {}
+
+    for i in range(0, num_passes):
+        z1 = train_dataset.dot(W1) + b1
+        a1 = np.tanh(z1)
+        z2 = a1.dot(W2) + b2
+        a2 = np.tanh(z2)
+        z3 = a2.dot(W3) + b3
+        exp_scores = np.exp(z3)
+        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+
+        delta4 = (probs - train_labels) / data_size
+        dW3 = (a2.T).dot(delta4)
+        db3 = np.sum(delta4, axis=0, keepdims=True)
+        delta3 = delta4.dot(W3.T) * (1 - np.power(a2, 2))
+        dW2 = (a1.T).dot(delta3)
+        db2 = np.sum(delta3, axis=0, keepdims=True)
+        delta2 = delta3.dot(W2.T) * (1 - np.power(a1, 2))
+        dW1 = np.dot(train_dataset.T, delta2)
+        db1 = np.sum(delta2, axis=0)
+
+        dW3 += reg_lambda * W3
+        dW2 += reg_lambda * W2
+        dW1 += reg_lambda * W1
+
+        W1 += -epsilon * dW1
+        b1 += -epsilon * db1
+        W2 += -epsilon * dW2
+        b2 += -epsilon * db2
+        W3 += -epsilon * dW3
+        b3 += -epsilon * db3
+        
+        model = { 'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2, 'W3': W3, 'b3': b3}
+        
+        if print_loss and i % 1000 == 0:
+            print("Loss after iteration %i: %f" %(i, calculate_loss(model)))
+    
+    return model
+ 
+ 
+model = build_model(128, num_passes = 5000, print_loss=True)
+
+def print_accuracy(model):
+    total = 0.
+    for i in range(len(train_dataset)):
+        if (predict(model, train_dataset[i])[0].astype('int64') == np.nonzero(train_labels)[1][i].astype('int64')):
+            total += 1.
+    return total/len(train_dataset)*100
+    
+print_accuracy(model)
